a5e0623d9538fca8ca101db4d4be4fa12617a4e0
Eric Gassel <ericgassel12@gmail.com>
2021-02-02 17:59:18 -0700

    changes

--- olympics-api/olympics-api.py
+++ olympics-api/olympics-api.py
@@ -8,17 +8,15 @@ import psycopg2
 
 app = flask.Flask(__name__)
 
-def establish_database_connection():
-<<<<<<< HEAD
 
-    database = 'olympics'
-    user = ''
-    password = ''
-=======
->>>>>>> 499d5a04c51c94c8f8f4b64146539d9b00e3416e
+def establish_database_connection():
     
+    from config import database
+    from config import user
+    from config import password
+
     try:
-        connection = psycopg2.connect(database='olympics', user='', password='')
+        connection = psycopg2.connect(database=database, user=user, password=password)
     except Exception as e:
         print(e)
         exit()
@@ -52,15 +50,11 @@ def get_games():
 
 @app.route('/nocs')
 def get_nocs():
-    #make connection
+    
     connection = establish_database_connection()
     cursor = connection.cursor()
 
-<<<<<<< HEAD
     query = "SELECT abbre, name FROM nocs"
-=======
-    query = "SELECT NOC, team FROM athletes_games"
->>>>>>> 499d5a04c51c94c8f8f4b64146539d9b00e3416e
 
     try:
         cursor.execute(query)
@@ -70,19 +64,48 @@ def get_nocs():
 
     noc_list = []
     for noc in cursor:
-<<<<<<< HEAD
         abbreviation = noc[0]
         country = noc[1]
-=======
-        
-        abbreviation = noc[len(noc)-1]
-        country = noc[len(noc)-2]
->>>>>>> 499d5a04c51c94c8f8f4b64146539d9b00e3416e
         noc_list.append({'noc':abbreviation, 'country':country})
     
     connection.close()
 
     return json.dumps(noc_list)
+
+@app.route('/medalists/games/<games_id>')
+def get_medals_winner_list(games_id):
+
+    connection = establish_database_connection()
+    cursor = connection.cursor()
+
+    query = 'SELECT athlete.id, athlete.name, athlete.sex, events.sports, events.events, athletes_total.medal, games.id, athletes_game.NOC FROM athletes, games, events, athletes_games, athletes_total, noc WHERE athlete_games.athletes_id = athletes_total.athletes_games_id AND athletes.id = athletes_games.athletes_id AND events.id = athletes_total.events_id;'
+
+    try:
+        cursor.execute(query)
+    except Exception as e:
+        print (e)
+        exit()
+    
+    games_list = []
+    noc_request = flask.request.args.get('noc')
+    
+    for game in cursor:
+        athlete_id = game[0]
+        athlete_name = game[1]
+        sex = game[2]
+        sport = game[3]
+        events = game[4]
+        medal = game[5]
+        searched_game = game[6]
+        noc = game[7]
+        if searched_game == games_id:
+            games_list.append({'athlete_id':athlete_id, 'athlete_name':athlete_name, 'sex': sex, 'sport':sport, 'event':events, 'medal':medal})
+        
+        
+        connection.close()
+        
+    return json.dumps(games_list)
+    
 
 def main():
     
--- olympics-api/olympics.sql
+++ olympics-api/olympics.sql
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 CREATE TABLE athletes (
     id INT,
     names text,
@@ -30,7 +29,7 @@ CREATE TABLE athletes_games (
 CREATE TABLE athletes_total(
     id INT,
     athletes_games_id INT,
-    events INT,
+    events_id INT,
     medal text
 );
 
@@ -46,52 +45,3 @@ CREATE TABLE nocs(
 \copy athletes_games from 'athletes_games.csv' DELIMITER ',' CSV NULL AS 'NA' HEADER;
 \copy athletes_total from 'athletes_total.csv' DELIMITER ',' CSV NULL AS 'NA' HEADER;
 \copy nocs from 'noc_regions.csv' DELIMITER ',' CSV NULL AS 'NULL' HEADER; 
-=======
-CREATE TABLE athletes (
-    id INT,
-    names text,
-    sex text,
-    weight INT
-);
-
-CREATE TABLE games(
-    id INT,
-    years INT,
-    season text,
-    city text
-);
-
-CREATE TABLE events(
-    id INT,
-    events text,
-    sports text
-);
-
-CREATE TABLE athletes_games (
-    id INT,
-    athletes_id INT,
-    games_id INT,
-    NOC text,
-    team text
-);
-
-CREATE TABLE athletes_total(
-    id INT,
-    athletes_games_id INT,
-    events INT,
-    medal text
-);
-
-CREATE TABLE nocs(
-    abbre TEXT,
-    name TEXT,
-    notes TEXT
-);
-
-\copy athletes from 'athletes.csv' DELIMITER ',' CSV NULL AS 'NA' HEADER;
-\copy games from 'games.csv' DELIMITER ',' CSV NULL AS 'NA' HEADER;
-\copy events from 'events.csv' DELIMITER ',' CSV NULL AS 'NA' HEADER;
-\copy athletes_games from 'athletes_games.csv' DELIMITER ',' CSV NULL AS 'NA' HEADER;
-\copy athletes_total from 'athletes_total.csv' DELIMITER ',' CSV NULL AS 'NA' HEADER;
-\copy nocs from 'noc_regions.csv' DELIMITER ',' CSV NULL AS 'NULL' HEADER; 
->>>>>>> 499d5a04c51c94c8f8f4b64146539d9b00e3416e
