494e00a0ae7b55f296035a5cf88d0c622c90e8d7
Eric Gassel <ericgassel12@gmail.com>
2021-02-09 03:38:10 -0700

    renamed

---
+++ database/olympics.sql
@@ -0,0 +1,37 @@
+CREATE TABLE athletes (
+    id INT,
+    names text,
+    sex text,
+    weight INT
+);
+
+CREATE TABLE games(
+    id INT,
+    years INT,
+    season text,
+    city text
+);
+
+CREATE TABLE events(
+    id INT,
+    events text,
+    sports text
+);
+
+
+CREATE TABLE athletes_games (
+    id INT,
+    athletes_id INT,
+    games_id INT,
+    NOC text,
+    team text
+);
+
+CREATE TABLE athletes_total(
+    id INT,
+    athletes_games_id INT,
+    events INT,
+    medal text
+);
+
+
